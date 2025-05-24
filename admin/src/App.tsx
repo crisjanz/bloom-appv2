8a4271599d258c9aa02aeff0635c72222a8778b5
Cristian Janz <cristianjanz@Cristians-MacBook-Air.local>
2025-05-24 15:57:36 -0700

    Set up product list page in admin panel

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,10 +1,16 @@
 import ProductList from './components/ProductList';
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+
 function App() {
   return (
-    <div className="p-6">
-      <ProductList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/products" element={<ProductsPage />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ admin/src/pages/ProductsPage.tsx
@@ -0,0 +1,9 @@
+import ProductList from "../components/ProductList";
+
+export default function ProductsPage() {
+  return (
+    <div className="p-6">
+      <ProductList />
+    </div>
+  );
+}
